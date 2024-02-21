68d1c44f8ac5d367b83dae30f93611c4c7cc768d
Francis Chryss Pegenia <chrysspegenia@gmail.com>
2024-02-21 18:41:24 +0800

    Rename encode_decode to the_vowel_code

---
+++ lesson-activities/the_vowel_code.rb
@@ -0,0 +1,40 @@
+# def encode(str)
+#     str.gsub("1", "a").gsub("2", "e").gsub("3", "i").gsub("4", "o").gsub("5", "u")
+# end
+
+# def decode(str)
+#     str.gsub("a", "1").gsub("e", "2").gsub("i", "3").gsub("o", "4").gsub("u", "5")
+# end
+
+# def encode(str)
+#     str.tr("aeiou", "12345")
+# end
+
+# def decode(str)
+#     str.tr("12345", "aeiou")
+# end
+
+VOWELS = {
+    "a" => 1,
+    "e" => 2,
+    "i" => 3,
+    "o" => 4,
+    "u" => 5,
+}
+
+def encode(str)
+    str.chars.map do |char|
+        VOWELS[char] || char
+    end.join
+end
+
+
+def decode(str)
+    str.chars.map do |char|
+        VOWELS.key(char.to_i) || char
+    end.join
+end
+
+puts encode("hello") # h2ll4
+
+puts decode("h3 th2r2") #  hi there
